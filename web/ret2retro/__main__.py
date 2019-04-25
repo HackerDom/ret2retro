da38006420ac7aed54c34acfb4daf99a689a4959
Arseny Lezin <arseny.lezin@gmail.com>
2019-04-25 18:28:53 +0500

    regex fix

--- web/ret2retro/__main__.py
+++ web/ret2retro/__main__.py
@@ -25,7 +25,7 @@ def main():
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(TEMPLATES_PATH))
     app.add_routes([
         web.post('/', transform_image),
-        web.get('/{image_hash}.png', glitched_image),
+        web.get('/{image_hash:[\w\d]+}.png', glitched_image),
         web.get('/', index),
     ])
     if not IS_PRODUCTION:
